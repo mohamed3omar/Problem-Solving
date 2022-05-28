aaa56ff531082416cdf25f64ff968a508fe06880
mhmd 3omar <mohamed3mar2019@gmail.com>
2022-05-28 17:38:18 +0200

    Memo momo problem

---
+++ Contest 1/Memo_and_Momo.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    long long a , b , k ;
+
+    cin>> a >> b >> k;
+    
+    if( (a % k == 0 )&&( b % k == 0 ))
+    {
+        cout<<"Both";
+
+    }
+    else if( (a % k == 0) &&(b % k != 0))
+    {
+        cout<<"Memo";
+    }
+    else if( (b % k == 0) && ( a % k != 0) )
+    {
+        cout<<"Momo";
+    }
+    else{
+        cout<<"No One";
+    }
+
+    return 0 ;
+}
