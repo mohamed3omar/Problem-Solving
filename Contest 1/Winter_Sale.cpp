0a57c9c222644b3cd9d4ddc4c947420f22afd103
mhmd 3omar <mohamed3mar2019@gmail.com>
2022-05-28 17:12:28 +0200

    winter sale

---
+++ Contest 1/Winter_Sale.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+
+    
+    //priceAfter_discount = Origprice - Oerignprie * Discount;
+
+    double X , P;
+
+    cin>> X >> P;
+    
+    cout<<fixed<<setprecision(2)<<((P/(100-X))*X)+P;
+
+    return 0;
+}
