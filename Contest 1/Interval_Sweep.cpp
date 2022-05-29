60646859048c84cb2045254717078e8aef6962a2
mhmd 3omar <mohamed3mar2019@gmail.com>
2022-05-29 20:41:50 +0200

    New Problem

---
+++ Contest 1/Interval_Sweep.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int a , b ;
+
+    cin>> a >> b ;
+
+    if( ( a == 0 && b == 0) || abs( a - b) > 1 )
+    {
+        cout<<"NO";
+    }
+    else{
+        cout<<"YES";
+    }
+
+    return 0 ;
+}
