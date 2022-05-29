5ef7dfb53c532021c9e352c3107ac7ff6c30fc6e
mhmd 3omar <mohamed3mar2019@gmail.com>
2022-05-29 21:06:26 +0200

    New Problem

---
+++ Contest 1/Adding_bits.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    long long a , b , result ;
+
+    
+    cin >>  a >> b ;
+
+    // operator ^ turn from decimal to binary then sum..
+    result = a ^ b;
+
+    cout<< result;
+
+
+    return 0;
+}
