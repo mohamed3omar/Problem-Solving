1b7ac2a2e509d6fae7b51bbf274335909b399f90
mhmd 3omar <mohamed3mar2019@gmail.com>
2022-06-06 21:27:26 +0200

    new prob

---
+++ Assiut_contest sheet lev2/Digits.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+
+    int cases;
+
+    int num;
+
+    cin>> cases;
+
+    string reverse;
+
+    for (int i = 0; i < cases; i++)
+    {
+        cin>>num;
+
+        
+        
+
+    }
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0 ;
+}
