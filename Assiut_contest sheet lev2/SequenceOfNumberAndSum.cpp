5cfda0710793240fc3f2da98d0ec4d4adceb1790
mhmd 3omar <mohamed3mar2019@gmail.com>
2022-06-07 13:45:11 +0200

    New Problem

---
+++ Assiut_contest sheet lev2/SequenceOfNumberAndSum.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int m , n ;
+
+    int max , min ;
+
+    int sum = 0 ;
+
+    int flag = true;
+
+    while (flag)
+    {  
+        cin>> m >> n;
+        
+        if( n <= 0 || m <= 0 )
+        {
+            flag = false;      
+        }
+        else{
+        if( m > n ){
+
+            max = m;
+
+            min = n;
+        }
+        else{
+            max = n;
+
+            min = m;
+        }
+        //print number between n and m by using min , max
+        for (int i = min; i <= max; i++)
+        {   
+            sum += i;
+
+            cout<< i << " " ;
+        }
+        cout<<"sum ="<<sum<<endl;
+        //reset the sum 
+        sum = 0 ;
+        }
+        
+    }
+    
+
+
+
+
+
+    return 0 ;
+}
