a6f32c3f932d5bf20a971a276be41372ce99365b
mhmd 3omar <mohamed3mar2019@gmail.com>
2022-06-07 13:05:57 +0200

    complet prob

--- Assiut_contest sheet lev2/Digits.cpp
+++ Assiut_contest sheet lev2/Digits.cpp
@@ -1,40 +1,29 @@
 #include<iostream>
 #include<string>
+#include<sstream>
 using namespace std;
 
 int main(){
 
-    int cases;
-
-    int num;
+    int  cases;
 
     cin>> cases;
+    //we can also use another arrya and assign it with reversed indexes
+    string number;
 
-    string reverse;
+    for (int i = 1; i <=cases; i++)
+    {   
+        //STRING IS ARRAY OF CHAR ..NOTE THAT
+        cin>>number;
 
-    for (int i = 0; i < cases; i++)
-    {
-        cin>>num;
+        for (int i = number.size()-1; i >=0; i--)
+        {   
+            cout<<number[i]<<" ";
+        }
 
-        
-        
-
+        cout<<endl;        
     }
     
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     return 0 ;
 }
