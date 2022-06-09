7f87cbd7fa0ecfe0fc5b13e30e9c7d0bc10fa773
mhmd 3omar <mohamed3mar2019@gmail.com>
2022-06-09 22:34:56 +0200

    new prob

---
+++ Contest 2/TimonAndPumbaa.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int number1 , number2 , result;
+
+    cin>> number1 >> number2 ;
+++
+    result = (number1 - number2);
+
+    if( result > 0 )
+    {
+        cout<< result;
+    }
+    else
+    {
+        cout<<0 ;
+    }
+}
