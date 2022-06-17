719b02d6a8facc6ec5f6a52e204e262407269af1
mhmd 3omar <mohamed3mar2019@gmail.com>
2022-06-17 11:42:54 +0200

    New Prob

--- Contest 2/RangeSum.cpp
+++ Contest 2/RangeSum.cpp
@@ -4,7 +4,31 @@ using namespace std;
 
 int main(){
 
+    long long tests ;
+
+    cin>> tests ;
+
+    long long num1 , num2 ;
+
+    while( tests--)
+    {
+        long long minimum , maximum;
+
+        cin>> num1 >> num2 ;
+        
+        maximum = max(num1 , num2);
+
+        minimum = min(num1 , num2);
+
+        minimum--;
+
+        long long result1 = minimum *(minimum + 1)/2 ;
+
+        long long result2 = maximum *(maximum + 1)/2;
+
+        cout<<result2 - result1<<endl;
     
+    }
 
 
     
