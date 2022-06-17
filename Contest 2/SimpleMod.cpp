5fae64e35cd1d182b5e13f8b3e40e5f8b624cb04
mhmd 3omar <mohamed3mar2019@gmail.com>
2022-06-17 15:06:11 +0200

    New Prob

---
+++ Contest 2/SimpleMod.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int num  ;
+    //(x^2 + y^2) mod num = 0 ;
+    cin>> num;
+    //lol this is the answer because 0 mod anynumber = 0 ;
+    cout<<"0 0"<<endl;
+    
+}
