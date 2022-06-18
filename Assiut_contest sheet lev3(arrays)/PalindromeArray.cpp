4729ecf2c6515f6fcecc87fc7d04672bbc9da908
mhmd 3omar <mohamed3mar2019@gmail.com>
2022-06-18 09:42:25 +0200

    New Prob

---
+++ Assiut_contest sheet lev3(arrays)/PalindromeArray.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int elements ;
+
+    cin >> elements;
+
+    long long numbers[elements];
+
+    long long compare[elements];
+
+    int counter = 0 ;
+
+    for (int i = 0; i < elements; i++)
+    {
+        cin >> numbers[i];
+    }
+    for (int j = elements-1; j >=0; j--)
+    {
+        compare[counter] = numbers[j];
+        counter++;
+    }
+    int flag = 0 ;
+    for (int i = 0; i < elements; i++)
+    {   
+        if(compare[i] != numbers[i])
+        {
+            cout<<"NO";
+            break;
+        }
+        if( compare[i] == numbers[i]  )
+        {    
+            
+            flag++;
+            
+            if( flag == elements)
+            {
+                cout<<"YES";
+            }
+        }
+    }
+    
+    
+    
+}
