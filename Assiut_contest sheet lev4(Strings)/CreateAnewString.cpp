982aa6cb042718f2e5fdfb5cd38980b60ecf749b
mhmd 3omar <mohamed3mar2019@gmail.com>
2022-06-23 13:26:42 +0200

    New Problem

---
+++ Assiut_contest sheet lev4(Strings)/CreateAnewString.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string str1 , str2 ;
+
+    cin >> str1  >> str2;
+
+    cout<<str1.size()<<" "<< str2.size()<<endl;
+    
+    cout<<str1<<" " <<str2;
+    
+}
