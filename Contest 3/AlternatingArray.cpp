e70eaf0358bc0e4b92775140fbc5bfe3d0ce856f
mhmd 3omar <mohamed3mar2019@gmail.com>
2022-06-23 13:13:40 +0200

    New prob

--- Contest 3/AlternatingArray.cpp
+++ Contest 3/AlternatingArray.cpp
@@ -9,15 +9,53 @@ int main()
     cin >> size;
 
     long long arr[size];
+    long long arr2[size];
+    
+    for (int i = 0; i < size; i++)
+    {  
+        int value;
+
+        cin>> value;
+
+        arr[i] = value;
+
+        arr2[i] = value;
+    }
+
+    int temp = 0 , counter = 0 , counter2 = 1 , temp2 = 0 ;
+
+    arr2[0] *= -1;
 
     for (int i = 0; i < size; i++)
-    {
-        cin >> arr[i]; 
+    {   
+        if( i == 0 )
+        {
+            temp = arr[i] ;
+            temp2 = arr2[i] ;
+        }
+        else{
+            if(( temp > 0 && arr[i] > 0 )||( temp < 0 && arr[i] < 0 ))
+            {
+                arr[i] *= -1;
+                counter++;
+            }
+            if(( temp2 > 0 && arr2[i] > 0 ) ||( temp2 < 0 && arr2[i] < 0 ))
+            {
+                arr2[i] *= -1;
+                counter2++;
+            }
+            temp = arr[i] ;
+            temp2 = arr2[i] ;
+        }
     }
-    for (int i = 0; i < size; i++)
+    if( counter < counter2  )
     {
-        
+        cout<< counter;
     }
+    else{
+        cout<< counter2;  
+    }
+    
     
     
 }
