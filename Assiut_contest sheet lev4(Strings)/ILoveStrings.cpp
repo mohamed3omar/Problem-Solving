7e089b7a84f2beb03c0bc6eaf0f53e5219102508
mhmd 3omar <mohamed3mar2019@gmail.com>
2022-06-28 17:35:48 +0200

    update problem

--- Assiut_contest sheet lev4(Strings)/ILoveStrings.cpp
+++ Assiut_contest sheet lev4(Strings)/ILoveStrings.cpp
@@ -1,1 +1,43 @@
+#include<iostream>
+#include<iostream>
  
+using namespace std;
+ 
+int main()
+{
+    int cases;
+ 
+    cin >> cases;
+ 
+    while (cases--)
+    {
+        string str1 , str2 ;
+ 
+        cin >> str1 >> str2;
+ 
+        int size = str2.size() + str1.size();
+        
+        int flag1  = 1 , flag2 = 1 ;
+        for (int i = 0; i < size; i++)
+        {   
+            if( str1.size() != i && flag1)
+            {
+                cout<<str1[i] ;
+            }
+            else{
+                flag1 = 0 ;
+            }
+            if( str2.size() != i && flag2)
+            {
+                cout<<str2[i] ;
+            }
+            else {
+                flag2 = 0 ;
+            }
+
+        }
+        cout<<endl;
+        
+    }
+    
+}
