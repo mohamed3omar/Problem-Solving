2221786b8f100ff917a7a9d2e1a6525fc8dca1f8
mhmd 3omar <mohamed3mar2019@gmail.com>
2022-07-02 20:35:18 +0200

    edit

--- Assiut_contest sheet lev4(Strings)/Comparison.cpp
+++ Assiut_contest sheet lev4(Strings)/Comparison.cpp
@@ -4,11 +4,31 @@ using namespace std;
 
 int main()
 {
-    string str;
+    //there are built in func erase(p1 , p2);
+    string str , sub1 = "" , sub2;
 
     cin >> str;
 
-    //
+    string myvalue = str;
+
+    string smallstring = str; 
+
+    for (int i = 0; i < str.length()-1; i++)
+    {
+        sub1 += str[i] ;
+
+        myvalue.erase(0,1);
+
+        sub2 = myvalue ;
+
+        sort(sub1.begin(),sub1.end());
+
+        sort(sub2.begin(),sub2.end());
+
+        smallstring = min(smallstring, sub1+sub2);
+    }
+    
+    cout<<smallstring <<endl;
 
 
 
