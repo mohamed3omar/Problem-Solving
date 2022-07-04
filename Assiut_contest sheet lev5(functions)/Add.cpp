e89f5b2fac915a6af50981f5fd7fa467d2452e81
mhmd 3omar <mohamed3mar2019@gmail.com>
2022-07-04 22:08:51 +0200

    New Problem

---
+++ Assiut_contest sheet lev5(functions)/Add.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+void add( int &x , int & y)
+{
+    cout<< x + y << endl;
+}
+
+int main()
+{
+    int x , y ;
+
+    cin >> x >> y ;
+
+    add( x , y ) ;
+}
