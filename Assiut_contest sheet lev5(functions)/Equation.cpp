d7f010de9ce70199b3cf74a66e860235f5b09b8f
mhmd 3omar <mohamed3mar2019@gmail.com>
2022-07-06 00:02:35 +0200

    just test

---
+++ Assiut_contest sheet lev5(functions)/Equation.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int equation(int x, int n)
+{
+    int res = -1;
+
+    if( n % 2 == 1 )
+    {
+        n-- ;
+    }
+    
+
+
+    
+
+}
+
+int main()
+{
+    int x , y ;
+
+    cin >> x >> y;
+
+    equation( x , y ) ;
+}
