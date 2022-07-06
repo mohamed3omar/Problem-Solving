742e228186db454c85de1e2e4f0bf6702ab4f768
mhmd 3omar <mohamed3mar2019@gmail.com>
2022-07-06 18:12:43 +0200

    changes

--- Assiut_contest sheet lev5(functions)/Equation.cpp
+++ Assiut_contest sheet lev5(functions)/Equation.cpp
@@ -5,15 +5,13 @@ using namespace std;
 void equation(long long x, long long n)
 {
     long long result = 0 ;
+    
     for(int i = 2 ; i <= n ; i+=2)
     {
         result += pow(x, i);
     }
     
-
     cout<< result << endl;
-    
-
 }
 
 int main()
