d6cde2525bf617a0a5d85e62a947afe9f3b63580
mhmd 3omar <mohamed3mar2019@gmail.com>
2022-07-20 00:23:54 +0200

    test

---
+++ Assiut_contest sheet lev7(Recursion)/SumOfMatrix.cpp
@@ -0,0 +1,37 @@
+// #include<bits/stdc++.h>
+
+// using namespace std;
+
+// void sum ( )
+// {
+
+// }
+
+// int main()
+// {
+//     //row and column
+//     int r , c ;
+
+//     cin >> r >> c ;
+
+//     long long arrA[r][c];
+
+//     long long arrB[r][c];
+
+//     for(int i = 0 ; i < r ; i++)
+//     {
+//         for(int j = 0 ; j < c ; j++)
+//         {
+//             cin>> arrA[i][j] ;
+//         }
+
+//     }
+//     for(int i = 0 ; i < r ; i++)
+//     {
+//         for(int j = 0 ; j < c ; j++)
+//         {
+//             cin>> arrB[i][j] ;
+//         }
+
+//     }
+// }
