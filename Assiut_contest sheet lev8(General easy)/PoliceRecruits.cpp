077517343be6ad229302301ef6a7dfbccac17cf6
mhmd 3omar <mohamed3mar2019@gmail.com>
2022-07-27 20:59:56 +0200

    New problem

---
+++ Assiut_contest sheet lev8(General easy)/PoliceRecruits.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int number ;
+
+    cin >> number ;
+
+    int pol = 0 , isu = 0  , num;
+
+    for (int i = 1; i <= number; i++)
+    {
+        cin >> num ;
+
+        if( num == -1 )
+        {
+            if( pol > 0 )
+            {
+                pol--;
+            }
+            else{
+                isu++;
+            }
+        }
+        else
+        {
+            pol+= num;
+        }
+    }
+    cout<<isu;
+    
+
+
+}
