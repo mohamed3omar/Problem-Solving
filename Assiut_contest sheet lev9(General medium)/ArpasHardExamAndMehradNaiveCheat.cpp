e940f4827e9592f25cc35d544e0c74da1d3b13da
mhmd 3omar <mohamed3mar2019@gmail.com>
2022-08-13 11:54:30 +0200

    New problem

---
+++ Assiut_contest sheet lev9(General medium)/ArpasHardExamAndMehradNaiveCheat.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+
+using namespace std; 
+
+int main()
+{
+    long long n  ;
+
+    cin >> n ;
+
+    if(n == 0 )
+    {
+        cout<<1;
+        return 0 ;
+    }
+    if( n % 4 == 0 )
+    {
+        cout<<6 <<endl;
+    }
+    else if ( n % 4 == 1 ) 
+    {
+        cout<<8 <<endl;
+    }
+    else if( n % 4 == 2 )
+    {
+        cout<<4 <<endl;
+    }
+    else if ( n % 2 == 3 )
+    {
+        cout<<2 <<endl;
+    }
+    /* 
+        note 1 % 4 = 1 ;
+             2 % 4 = 2 ;
+             3 % 4 = 3 ;
+             4 % 4 = 0 ;
+             5 % 4 = 1 ; ah shit here we go again -_=
+
+             same last digit 1- 1378^1 = 8
+                             2- 1378^2 = 4
+                             3- 1378^3 = 2 
+                             4- 1378^4 = 6
+                             5- 1378^5 = 8
+                             6- 1378^6 = 4 ah shit here we go again -_-
+
+    */
+
+    // another sol but not always true;
+    // long long n ;
+
+    // cin >> n ;
+
+    // long long res = pow(1378 , n ) ;
+
+    // cout<< (res % 10);
+}
